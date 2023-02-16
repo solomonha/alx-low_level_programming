888d4a182b852e6280418a468f0bb10d5e3896be
solomonha <hsol37@gmail.com>
2023-02-16 14:10:24 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-fputs(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", stdout);
 return (1);
 }
