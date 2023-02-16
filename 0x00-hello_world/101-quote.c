bf15bd9e5561e3d229bdb9d21445e4fe18835bdd
solomonha <hsol37@gmail.com>
2023-02-16 14:02:30 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line to the standard error
+ * return: 1 (error)
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+return (1);
+}
