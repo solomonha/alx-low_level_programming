99d6d48abd87f077f54b9faa87e009c6ea872d38
solomonha <hsol37@gmail.com>
2023-02-17 08:55:39 -0800

    6-print-number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A program that prints base 10 numbers using putchar
+ * Return: 0 (success)
+ */
+int main(void)
+{
+        int number;
+
+        for (number = '0'; number <= '9'; number++)
+                putchar(number);
+        putchar('\n');
+        return (0);
+}
