6da0a0cb7160015159bcd09528a38068391061fc
solomonha <hsol37@gmail.com>
2023-02-17 08:29:16 -0800

    print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,7 +16,7 @@ int main(void)
 
 	for  (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-	for  (ch = 'a'; ch <= 'z'; ch++)
+	for  (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
