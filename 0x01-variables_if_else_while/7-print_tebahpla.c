2d1f69c7957de3b50b06440a1564a23d49ee0d3b
solomonha <hsol37@gmail.com>
2023-02-17 09:05:27 -0800

    tebalpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A program that prints lowercase except e and q
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch <= 'z'; ch = 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
