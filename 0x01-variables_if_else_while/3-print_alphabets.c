d1d7a61bea60546658d45845337ced4e73338d50
solomonha <hsol37@gmail.com>
2023-02-17 07:38:28 -0800

    print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - A program that prints lowercase characters
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+	
+	for  (ch = 'a'; ch <= 'z'; ch++)
+                putchar(ch);
+	for  (ch = 'a'; ch <= 'z'; ch++)
+                putchar(toupper(ch));
+        putchar('\n');
+        return (0);
+}
