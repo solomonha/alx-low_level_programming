b1b18da3c079a15ce8b2e62056953db3eee51de2
solomonha <hsol37@gmail.com>
2023-02-17 08:59:15 -0800

    6-print-number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,10 +9,10 @@
  */
 int main(void)
 {
-        int number;
+	int number;
 
-        for (number = '0'; number <= '9'; number++)
-                putchar(number);
-        putchar('\n');
-        return (0);
+	for (number = '0'; number <= '9'; number++)
+		putchar(number);
+	putchar('\n');
+	return (0);
 }
