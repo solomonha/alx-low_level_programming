0f783c4e4ed11606438c958b0de7845d56355866
solomonha <hsol37@gmail.com>
2023-02-17 11:36:01 -0800

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints decimal numbers separeted by comma and space
+ * Return:0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+	putchar('$');
+	return (0);
+}
