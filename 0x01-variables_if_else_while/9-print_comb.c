af1e4391b4969157e99f677a9ead1bb026b1c059
solomonha <hsol37@gmail.com>
2023-02-17 12:05:29 -0800

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,9 +10,12 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
+	}
+	for (num = '0'; num < '9'; num++)
+	{
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('$');
+	putchar('\n');
 	return (0);
 }
