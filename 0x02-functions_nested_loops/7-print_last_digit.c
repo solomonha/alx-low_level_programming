ec4a3fcae7079fc90cd4d72758d1310afc740cca
solomonha <hsol37@gmail.com>
2023-02-21 12:58:57 -0800

    7-last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,12 @@ int print_last_digit(int n)
 {
 	int ld;
 
-	if (n >= 0)
-	{
-		ld = n % 10;
-	}
-	else
+	ld = n % 10;
+	if (n < 0)
 	{
 		ld = -(n % 10);
-		_putchar(ld + '0');
 	}
+	_putchar(ld + '0');
 	return (ld);
 }
 
