b47116fa3d27bd2c44205c351b4859fb0ec76a0d
solomonha <hsol37@gmail.com>
2023-02-22 02:10:42 -0800

    natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - computes and prints the sum of all the multiples of 3 or 5,
- *                      below 1024 (excluded),followed by a new line.
+ * main - prints the sum of all the multiples of 3 or 5,
+ *         below 1024 (excluded),followed by a new line.
  * return: 0 (success)
  */
 int main(void)
