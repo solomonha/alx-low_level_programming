030e6ab6db00d711acf47f4da22175a448671dc3
solomonha <hsol37@gmail.com>
2023-02-27 13:20:04 -0800

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,13 @@ void puts_half(char *str)
 	int i, j, len = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
-	{ 
+	{
 		len++;
 	}
 	j = (i / 2);
 	if ((j % 2) == 1)
 	{
-		j = ((len + 1)/2);
+		j = ((len + 1) / 2);
 	}
 	for (i = j; str[i] != '\0'; i++)
 	{
