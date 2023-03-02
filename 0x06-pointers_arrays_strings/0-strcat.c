02a5e9daaefd5d3edb55351aff77aae40652d3b3
solomonha <hsol37@gmail.com>
2023-03-01 23:50:49 -0800

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * _strcat - A function that concatenates two strings
  * @dest: string copy to
  * @src: string copy from
- * return:  a pointer to the resulting string dest
+ * Return: two concat strings
  */
 char *_strcat(char *dest, char *src)
 {
