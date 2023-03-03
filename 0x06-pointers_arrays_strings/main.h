95f776fedc6fc10f69d5757b00c0682d480603a7
solomonha <hsol37@gmail.com>
2023-03-03 04:34:25 -0800

    101-print_number

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,6 +13,6 @@ char *leet(char *s);
 char *rot13(char *s);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
+void print_number(int n);
 
 #endif /* MAIN_H */
