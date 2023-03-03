8e8712b431ba1d5b80b575ee8139142fc2fc720e
solomonha <hsol37@gmail.com>
2023-03-03 04:26:42 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,24 +4,25 @@
 /**
  * rot13 - a function that encodes a string using rot13
  * @s: pointer to string
- * return: Always 0 (Success)
+ * Return:0  Always (Success)
  */
 char *rot13(char *s)
 {
-        int i, j;
-        char input[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        char output[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i, j;
+	char input[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char output[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-        for (i = 0; s[i] != '\0'; i++)
-        {
-                for (j = 0; s[j] != '\0'; j++)
-                {
-                        if (s[i] == input[j])
-                        {
-                                s[i] = output[j];
-                                break;
-                        }
-                }
-        }
-        return (s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[i] == input[j])
+			{
+				s[i] = output[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,6 +11,8 @@ char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
 char *rot13(char *s);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 
 #endif /* MAIN_H */
