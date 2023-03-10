ae52c48231026d1aaf50fde37ab053ddf92949c8
solomonha <hsol37@gmail.com>
2023-03-10 07:23:42 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - function to print name of function followed by newline
+* @argc: unused argument
+*
+* @argv: argument whos first element [0] is the program name
+*
+* Return: (0)
+*/
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n ", argv[argc]);
+	return (0);
+
+}
