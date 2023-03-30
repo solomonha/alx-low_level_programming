fa4c07b702fe3a18521845a827519d33508121d2
solomonha <hsol37@gmail.com>
2023-03-30 12:10:35 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void first(void) __attribute__((constructor));
+
+/**
+ * first - prints phrase before main function is executed
+ *
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
