860b1896fcc624a8206c135205724f49b56ceaff
solomonha <hsol37@gmail.com>
2023-03-30 13:10:54 -0700

    updating function that prints all the elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,19 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t node = 0;
+	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		node++;
-		if (h->str != NULL)
-			printf("[%u] %s\n", h->len, h->str);
-		else
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
 		h = h->next;
 	}
-	return (node);
+
+	return (nodes);
 }
