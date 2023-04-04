c18f2cc2783147716b8e0f7c9567488ea63ffcd1
solomonha <hsol37@gmail.com>
2023-04-04 14:23:40 -0700

    fix some errors

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,11 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = (*head);
+	listint_t *temp;
 
 	if (head == NULL || *head == NULL)
 		return;
-	while (temp != NULL)
+	while (*head != NULL)
 	{
 		temp = temp->next;
 		free((*head));
