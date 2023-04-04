a3a3f4ea73896e8354bddfd73a641d9952afb31e
solomonha <hsol37@gmail.com>
2023-04-04 05:06:42 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,13 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp = (*head);
 
 	while (temp != NULL)
 	{
 		temp = temp->next;
 		free(*head);
-		*head = temp;
+		(*head) = temp;
 	}
+	head = NULL;
 }
