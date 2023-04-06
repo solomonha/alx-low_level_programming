e24bc372fea37cb8d44fb1118ca33a593f71625b
solomonha <hsol37@gmail.com>
2023-04-06 11:32:13 -0700

    fix error

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,7 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > (sizeof(*n) * 8))
 		return (-1);
 	/* 1ul - specifies an unsigned long version of 1 */
-	*n = *n | (1 << index);
+	*n = *n | 1 << index;
 	return (1);
 
 }
