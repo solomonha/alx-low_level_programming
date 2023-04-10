35acc07b5da4ee05687ac648cfac76cd685a27f7
solomonha <hsol37@gmail.com>
2023-04-10 15:52:56 -0700

    2nd try

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -83,10 +83,10 @@ int main(int argc, char *argv[])
 	{
 		size_r = read(file_from, buf, 1024);
 		if (size_r == -1)
-			return(read_errck(file_from, file_to, argv[1]));
+			return (read_errck(file_from, file_to, argv[1]));
 		size_w = write(file_to, buf, size_r);
 		if (size_w == -1 || size_w != size_r)
-			return(write_errck(file_from, file_to, argv[2]));
+			return (write_errck(file_from, file_to, argv[2]));
 	}
 	err = closefd_errck(file_from);
 	err += closefd_errck(file_to);
