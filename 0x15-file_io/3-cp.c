71ea0c56362dbf8a2e7c082c6b41b33f919bc0c9
solomonha <hsol37@gmail.com>
2023-04-10 16:11:54 -0700

    2nd try

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -73,8 +73,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -93,6 +92,6 @@ int main(int argc, char *argv[])
 	err = closefd_errck(file_from);
 	err += closefd_errck(file_to);
 	if (err != 0)
-		exit (100);
+		exit(100);
 	return (0);
 }
